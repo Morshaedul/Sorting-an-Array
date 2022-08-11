d9b0a4b95323688e690a33288cd3d2f11020e9d5
Morshaedul <morshaedulanirbanit@gmail.com>
2022-08-11 20:07:05 +0600

    first commit

---
+++ Js/index.js
@@ -0,0 +1,17 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo1").innerHTML = fruits;
+
+fruits.sort();
+document.getElementById("demo2").innerHTML = fruits;
+
+
+
+
+
+
+
+// var date =new Date();
+// var currentDate=date.getFullYear();
+// var Date=date.getDay();
+// document.write(currentDate);
+// document.write(date);
